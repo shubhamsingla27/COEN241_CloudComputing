13a13a962f2d7d2a8bde5a18a1180679d769bb4b
Shubham Singla <shubhamsingla@BBMacBook-Pro.attlocal.net>
2023-02-01 16:20:09 -0800

    changes in script file

--- HW1/sysbenchQEMU.sh
+++ HW1/sysbenchQEMU.sh
@@ -2,27 +2,6 @@ outputFile=$1
 sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
 touch $outputFile
 > $outputFile
-runCPU(){
-	number=$1
-	shift
-	for i in {1..5}; do
-		sysbench --test=cpu --cpu-max-prime=$number run >> $outputFile
-		echo "-----------------------------------------------" >> $outputFile
-	done
-}
-
-runFileIO(){
-	size=$1
-	mode=$2
-	shift
-	for i in {1..5}; do
-		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw prepare
-		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw run >> $outputFile
-		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw cleanup
-		sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-		echo "-----------------------------------------------" >> $outputFile
-	done
-}
 
 echo "CPU Configuration" >> $outputFile
 sudo lscpu | tee >(grep 'CPU(s):' >> $outputFile) >(grep 'Socket' >> $outputFile) >(grep 'Thread' >> $outputFile)
@@ -31,17 +10,40 @@ echo "Memory" >> $outputFile
 sudo lshw -c memory | grep capacity >> $outputFile
 echo "<<===CPU Tests===>>" >> $outputFile
 echo "Prime numbers limit: 10000" >> $outputFile
-runCPU 10000
+cpuTests 10000
 echo "Prime numbers limit: 20000" >> $outputFile
-runCPU 20000
+cpuTests 20000
 echo "Prime numbers limit: 30000" >> $outputFile
-runCPU 30000
+cpuTests 30000
 
 echo "<<<=====================================================>>>" >> $outputFile
 
 echo "<<===File IO Tests===>>" >> $outputFile
 echo "###### 2G 128Files 16Threads" >> $outputFile
-runFileIO 2G
+fileIoTests 2G
 echo "###### 4G 128Files 16Threads" >> $outputFile
-runFileIO 4G
+fileIoTests 4G
+
+cpuTests(){
+	number=$1
+	shift
+	for i in {1..5}; 
+	do
+		sysbench --test=cpu --cpu-max-prime=$number run >> $outputFile
+		echo "-----------------------------------------------" >> $outputFile
+	done
+}
+
+fileIoTests(){
+	size=$1
+	shift
+	for i in {1..5}; 
+	do
+		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw prepare
+		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw run >> $outputFile
+		sysbench --num-threads=16 --test=fileio --file-total-size=$size --file-test-mode=rndrw cleanup
+		sync; sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+		echo "-----------------------------------------------" >> $outputFile
+	done
+}
 
